92fb6e9f4df9c78483653dafb9b4ca689d5045e7
Kevin Ngo <me@ngokevin.com>
2019-02-25 13:35:11 -0800

    add ability to mark html includes as dep

--- html/index.js
+++ html/index.js
@@ -1,7 +1,30 @@
+const getOptions = require('loader-utils').getOptions;
 const Nunjucks = require('nunjucks');
+const path = require('path');
 
 const nunjucks = Nunjucks.configure(__dirname, {noCache: true});
 
+// Previous loaders can store <includes>COMMA-SEPARATED PATHS<end-includes>
+// to help Webpack detect changes (e.g, passed from super-nunjucks-loader).
+const includesRegex = /<includes>(.*)?<end-includes>/;
+const includeRootRegex = /<include-root>(.*)?<end-include-root>/;
+
 module.exports = function (source) {
+  let includeRoot = '';
+  const includeRootMatch = includeRootRegex.exec(source);
+  if (includeRootMatch) { includeRoot = includeRootMatch[1]; }
+
+  if (includeRoot) {
+    let includes = [];
+    const includesMatch = includesRegex.exec(source);
+    if (includesMatch) { includes = includesMatch[1].split(','); }
+
+    if (includes.length) {
+      includes.forEach(include => {
+        this.addDependency(path.resolve(includeRoot, include));
+      });
+    }
+  }
+
   return nunjucks.render('hot.template.js', {html: source});
 };
